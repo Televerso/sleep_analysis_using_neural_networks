e6b98a68a9555f4d184758bd210f5621747c5c1b
televerso <dbogat23@yandex.ru>
2026-05-20 00:19:48 +0400

    Cnn unit tests and passing

---
+++ tests/cnn_tests/SleepNet_tests/SleepNet_batch_test.py
@@ -0,0 +1,36 @@
+import os
+import unittest
+
+import cv2
+import numpy as np
+
+from src.cnn.SleepNet import predictor
+from src.utils.config_readers.ReaderConfig import ReaderConfig
+from src.utils.config_readers.ViBEConfig import ViBEConfig
+import src.video_processing.vibe_extractor.vibe_extractor as vibe_extractor
+from src.video_processing.input_reader import reader
+from tests.test_utils.MaskListAssertions import TestMaskListAssertions
+
+
+class SleepNet_single_test(unittest.TestCase):
+    def setUp(self):
+        ROOT_DIR = os.path.split(os.environ['VIRTUAL_ENV'])[0]
+        weights_path = os.path.join(ROOT_DIR, r"tests\cnn_tests\SleepNet_tests\input_data\weights.pth")
+
+        n = len(os.listdir(os.path.join(ROOT_DIR, r"tests/cnn_tests/SleepNet_tests/input_data/batch/")))
+        self.images = np.empty(shape=(n,64,64), dtype=np.uint8)
+        for i in range(n):
+            self.images[i] = cv2.imread(os.path.join(ROOT_DIR, r'tests\cnn_tests\SleepNet_tests\input_data\batch', f"{str(i)}.png"))[..., 0]
+
+        self.predictor = predictor.SleepNetPredictor(weights_path)
+
+    def test_batch(self):
+        results = self.predictor.predict_batch(self.images)
+        for i, res in enumerate(results):
+            self.assertEqual(i%3,res, msg=f"Mask with index {i} does not match for batch processing for cnn SleepNet")
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/cnn_tests/SleepNet_tests/SleepNet_single_test.py
@@ -0,0 +1,35 @@
+import os
+import unittest
+
+import cv2
+
+from src.cnn.SleepNet import predictor
+from src.utils.config_readers.ReaderConfig import ReaderConfig
+from src.utils.config_readers.ViBEConfig import ViBEConfig
+import src.video_processing.vibe_extractor.vibe_extractor as vibe_extractor
+from src.video_processing.input_reader import reader
+from tests.test_utils.MaskListAssertions import TestMaskListAssertions
+
+
+class SleepNet_single_test(unittest.TestCase):
+    def setUp(self):
+        ROOT_DIR = os.path.split(os.environ['VIRTUAL_ENV'])[0]
+        self.image0 = cv2.imread(os.path.join(ROOT_DIR, r'tests\cnn_tests\SleepNet_tests\input_data\single\0.png'))[..., 0]
+        self.image1 = cv2.imread(os.path.join(ROOT_DIR, r'tests\cnn_tests\SleepNet_tests\input_data\single\1.png'))[..., 0]
+        self.image2 = cv2.imread(os.path.join(ROOT_DIR, r'tests\cnn_tests\SleepNet_tests\input_data\single\2.png'))[..., 0]
+        weights_path = os.path.join(ROOT_DIR, r"tests\cnn_tests\SleepNet_tests\input_data\weights.pth")
+        self.predictor = predictor.SleepNetPredictor(weights_path)
+
+    def test_pose_0(self):
+        self.assertEqual(0,self.predictor.predict_single(self.image0), msg="Pose left_log for SleepNet for single frame processing does not match")
+
+    def test_pose_1(self):
+        self.assertEqual(1,self.predictor.predict_single(self.image1), msg="Pose right_log for SleepNet for single frame processing does not match")
+
+    def test_pose_2(self):
+        self.assertEqual(2,self.predictor.predict_single(self.image2),  msg="Pose supine for SleepNet for single frame processing does not match")
+
+
+
+if __name__ == '__main__':
+    unittest.main()

befb92c9ccd2a8a01312ae012067ac5a9da60490
televerso <dbogat23@yandex.ru>
2026-05-22 01:22:24 +0400

    Simple bg subtractor

---
+++ scripts/background_extraction_work/simple_bg_kesha_test.py
@@ -0,0 +1,22 @@
+import os
+
+import src.video_processing.background_extractor.simple_background_extractor as sb
+from src.utils.config_readers.ReaderConfig import ReaderConfig
+from src.utils.file_functions.save_frames import save_frames
+from src.video_processing.input_reader.reader import rsv_read_all
+
+if __name__ == '__main__':
+    ROOT_DIR = os.path.split(os.environ['VIRTUAL_ENV'])[0]
+    video_path = os.path.join(ROOT_DIR, r'inputs\kesha\video.mp4')
+    config = ReaderConfig().from_yaml(os.path.join(ROOT_DIR, r'config\config.yml'))
+    frames = rsv_read_all(video_path, config)
+
+
+
+    sb_ext = sb.SimpleBackgroundExtractor(frames[-1], 128)
+
+    masks = list()
+    for frame in frames:
+        masks.append(sb_ext.detect(frame))
+
+    save_frames(os.path.join(ROOT_DIR, r'outputs\background'), masks)
---
+++ src/video_processing/background_extractor/simple_background_extractor.py
@@ -0,0 +1,28 @@
+import numpy as np
+import src.utils.basic_functions.BasicFunctions as bf
+
+
+class SimpleBackgroundExtractor:
+    def __init__(self, image, threshold):
+        self.background_image = np.asarray(image)
+        self.background_image[:,:,0] = bf.blur(np.asarray(image[:,:,0]), 5, 1)
+        self.background_image[:,:,1] = bf.blur(np.asarray(image[:,:,1]), 5, 1)
+        self.background_image[:,:,2] = bf.blur(np.asarray(image[:,:,2]), 5, 1)
+
+        self.threshold = threshold
+
+    def detect(self, image):
+        curr_image = np.asarray(image)
+
+        curr_image[:,:,0] = bf.blur(image[:,:,0], 5, 1)
+        curr_image[:,:,1] = bf.blur(image[:,:,1], 5, 1)
+        curr_image[:,:,2] = bf.blur(image[:,:,2], 5, 1)
+
+        image_diff = np.abs(curr_image.astype(int) - self.background_image.astype(int))
+
+        image_diff[image_diff < self.threshold] = 0
+        image_diff[image_diff >= self.threshold] = 255
+
+        image_diff = image_diff[:,:,0] | image_diff[:,:,1] | image_diff[:,:,2]
+
+        return image_diff.astype('uint8')

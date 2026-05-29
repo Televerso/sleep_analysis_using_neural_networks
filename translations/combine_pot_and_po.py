33470ba01bec10ef628a378d08538372df6c4012
televerso <dbogat23@yandex.ru>
2026-05-30 00:31:20 +0400

    Refactored scripts for generating translations

--- translations/combine_pot_and_po.py
+++
@@ -1,5 +0,0 @@
-import os
-
-
-if __name__ == "__main__":
-    os.system("pybabel update -i sleep_analysis.pot -d . -l ru_RU -D sleep_analysis")
---
+++ translations/scripts/combine_pot_and_po.py
@@ -0,0 +1,5 @@
+import os
+
+
+if __name__ == "__main__":
+    os.system("pybabel update -i ../sleep_analysis.pot -d .. -l ru_RU -D sleep_analysis")
